077bc9324959abfb4f4799d7d2655a42c5151182
Devin Burke <devinburke@gmail.com>
2022-04-30 14:04:24 -0500

    added settings and schedule functionality

---
+++ src/schedule.js
@@ -0,0 +1,227 @@
+var time_settings = {
+	current_time: '',
+	current_element: 'hour',
+	current_hour: '',
+	current_minute: '',
+	current_type: '',
+	editing: false
+};
+
+var keypad_items = [
+	[7,8,9],
+	[4,5,6],
+	[1,2,3],
+	['AM',0,'PM']
+];
+
+var keypad_controls = [
+	[
+		{tag: 'direction', icon: 'arrow_left', action: 'left'},
+		{tag: 'done', icon: 'done', action: 'done'},
+		{tag: 'direction', icon: 'arrow_right', action: 'right'}
+	]
+];
+
+function keyPadEmit(value) {
+	if (value == 'AM' || value == 'PM') {
+		time_settings.current_type = value;
+		document.getElementsByTagName('time-type')[0].innerText = time_settings.current_type;
+		return;
+	}
+	var currentValue = '';
+	if (time_settings.editing) {
+		if (time_settings.current_element == 'hour')
+			currentValue = time_settings.current_hour;
+		else if (time_settings.current_element == 'minute')
+			currentValue = time_settings.current_minute;
+	}
+	else 
+		time_settings.editing = true;
+	if (currentValue == '' && value == '0' && time_settings.current_element != 'minute')
+		return;
+	currentValue += value;
+	if (time_settings.current_element == 'hour') {
+		if (parseInt(currentValue) > 12)
+			currentValue = '12';
+		time_settings.current_hour = currentValue;
+		document.getElementsByTagName('hour')[0].innerText = time_settings.current_hour;
+	}
+	else if (time_settings.current_element == 'minute') {
+		if (parseInt(currentValue) > 59)
+			currentValue = '59';
+		time_settings.current_minute = parseInt(currentValue).toString().padStart(2, '0');
+		document.getElementsByTagName('minute')[0].innerText = time_settings.current_minute;
+	}
+	if (currentValue.length > 1 || time_settings.current_element == 'hour' && value != '1')
+		keyPadControl('right');
+}
+
+async function keyPadControl(direction) {
+	if (direction == 'left') {
+		if (time_settings.current_element == 'hour')
+			return;
+		time_settings.current_element = 'hour';
+		var minute = document.getElementsByTagName('minute')[0];
+		minute.classList.remove('active');
+		var hour = document.getElementsByTagName('hour')[0];
+		time_settings.current_hour = '';
+		hour.classList.add('active');
+	}
+	else if (direction == 'right') {
+		if (time_settings.current_element == 'minute')
+			return;
+		time_settings.current_element = 'minute';
+		var hour = document.getElementsByTagName('hour')[0];
+		hour.classList.remove('active');
+		var minute = document.getElementsByTagName('minute')[0];
+		time_settings.current_minute = '';
+		minute.classList.add('active');
+	}
+	else {
+		var newTime = time_settings.current_hour + ':' + time_settings.current_minute.padStart(2, '0') + ' ' + time_settings.current_type;
+		document.getElementById(time_settings.current_time).innerText = newTime;
+		var modal = document.getElementsByTagName('modal')[0];
+		
+		var dayIndex = UserConfig.Schedule.Days.findIndex(d => d.Name.toLowerCase() == time_settings.current_time.split('-')[0]);
+		if (time_settings.current_time.split('-')[1] == 'start')
+			UserConfig.Schedule.Days[dayIndex].StartTime = newTime;
+		else 
+			UserConfig.Schedule.Days[dayIndex].EndTime = newTime;
+		await UserConfig.save();
+		modal.remove();
+	}
+}
+
+function createKeyPadButton(value) {
+	var button = document.createElement('key');
+	button.textContent = value;
+	button.onclick = function(e) {
+		keyPadEmit(e.target.textContent);
+	}; 
+	return button;
+}
+
+function createKeyPad() {
+	var keyPad = document.createElement('key-pad');
+	keypad_items.forEach(k => {
+		var row = document.createElement('row');
+		k.forEach(n => {
+			var btn = createKeyPadButton(n.toString());
+			if (n.toString() == 'AM' || n.toString() == 'PM')
+				btn.classList.add('time-type');
+			row.appendChild(btn);
+		});
+		keyPad.appendChild(row);
+	});
+	keypad_controls.forEach(k => {
+		var row = document.createElement('row');
+		k.forEach(n => {
+			var btn = document.createElement(n.tag);
+			var icon = document.createElement('i');
+			icon.classList.add('material-icons');
+			icon.textContent = n.icon;
+			btn.appendChild(icon);
+			btn.onclick = function(e) {
+				keyPadControl(n.action);
+			};
+			row.appendChild(btn);
+		});
+		keyPad.appendChild(row);
+	});
+	return keyPad;
+}
+
+function changeTime(id, value) {
+	var enabled = UserConfig.Schedule.Days.find(s => s.Name.toLowerCase() == id.split('-')[0]).Enabled;
+	if (!enabled)
+		return;
+		
+	var body = document.getElementsByTagName('body')[0];
+	var modal = document.createElement('modal');
+	modal.classList.add('hidden');
+		
+	var hour = '';
+	var minute = '';
+	var type = 'AM';
+	if (value != '' && value != null) {
+		hour = value.split(':')[0];
+		minute = value.split(':')[1].split(' ')[0];
+		type = value.split(':')[1].split(' ')[1];
+	}
+	
+	time_settings.current_time = id;
+	time_settings.current_hour = hour;
+	time_settings.current_minute = minute;
+	time_settings.current_type = type;
+	time_settings.editing = false;
+	time_settings.current_element = 'hour';
+	
+	var valueElement = document.createElement('time-entry');
+	var hourElement = document.createElement('hour');
+	hourElement.textContent = hour;
+	hourElement.classList.add('active');
+	valueElement.appendChild(hourElement);
+	var divisor = document.createElement('span');
+	divisor.textContent = ':';
+	valueElement.appendChild(divisor);
+	var minuteElement = document.createElement('minute');
+	minuteElement.textContent = minute;
+	valueElement.appendChild(minuteElement);
+	var typeElement = document.createElement('time-type');
+	typeElement.textContent = type;
+	valueElement.appendChild(typeElement);
+	modal.appendChild(valueElement);
+	modal.appendChild(createKeyPad());
+	
+	body.appendChild(modal);
+	setTimeout(function(){ 
+		document.getElementsByTagName('modal')[0].classList.remove('hidden');
+	}, 200);
+}
+
+function createTimeElement(id, value) {
+	var time = document.createElement('time');
+	time.id = id;
+	time.textContent = value;
+	time.onclick = function(e) {
+		changeTime(e.target.id, e.target.textContent);
+	};
+	return time;
+}
+
+async function toggleScheduleDay(day) {
+	var index = UserConfig.Schedule.Days.findIndex(s => s.Name == day);
+	if (index < 0)
+		return;
+		UserConfig.Schedule.Days[index].Enabled = !UserConfig.Schedule.Days[index].Enabled;
+	var element = document.getElementById('schedule-' + day.toLowerCase());
+	if (UserConfig.Schedule.Days[index].Enabled)
+		element.classList.add('selected');
+	else 
+		element.classList.remove('selected');
+	await UserConfig.save();
+}
+
+function setupSchedule() {
+	var scheduleContainer = document.getElementById('schedule-list');
+	UserConfig.Schedule.Days.forEach(s => {
+		var li = document.createElement('li');
+		var span = document.createElement('span');
+		span.textContent = s.Name;
+		span.onclick = (_) => toggleScheduleDay(s.Name);
+		li.appendChild(span);
+		li.id = 'schedule-' + s.Name.toLowerCase();
+		li.appendChild(document.createElement('flex'));
+		var from = createTimeElement(s.Name.toLowerCase() + '-start', s.StartTime);
+		var to = createTimeElement(s.Name.toLowerCase() + '-end', s.EndTime);
+		if (s.Enabled)
+			li.classList.add('selected');
+		else {
+			from.classList.add('disabled');
+			to.classList.add('disabled');
+		}
+		li.appendChild(from);
+		li.appendChild(to);
+		scheduleContainer.appendChild(li);
+	});
+}
---
+++ src/settings.js
@@ -0,0 +1,274 @@
+var container = document.getElementById('settings').querySelector('.card-primary').querySelector('.sub-container');
+var goalGroup;
+var parameterGroup;
+
+const ToggleType = 'Toggle';
+const TimeSpanType = 'TimeSpan';
+const GoalCategory = 'Goal';
+const ParameterCategory = 'Parameter';
+
+findSetting = function(id) {
+	var output = goalGroup.Settings.find(s => s.Id == id);
+	if (output != null) return output;
+	return parameterGroup.Settings.find(s => s.Id == id);
+}
+
+class Setting
+{
+	#id;
+	#name;
+	#description;
+	#value;
+	#type;
+	#category;
+	#element;
+	
+	constructor(id, name, description, value, category, type) 
+	{
+		this.#id = id;
+		this.#name = name;
+		this.#description = description;
+		this.#value = value;
+		this.#type = type;
+		this.#category = category;
+		
+		Object.defineProperties(this, {
+			Id: {
+				get: function() {
+					return this.#id;
+				},
+				set: function(value) {
+					this.#id = value;
+				}
+			},
+			Name: {
+				get: function() {
+					return this.#name;
+				},
+				set: function(value) {
+					this.#name = value;
+				}
+			},
+			Description: {
+				get: function() {
+					return this.#description;
+				},
+				set: function(value) {
+					this.#description = value;
+				}
+			},
+			Value: {
+				get: function() {
+					return this.#value;
+				},
+				set: function(value) {
+					this.#value = value;
+				}
+			},
+			Category: {
+				get: function() {
+					return this.#category;
+				},
+				set: function(value) {
+					this.#category = value;
+				}
+			},
+			Type: {
+				get: function() {
+					return this.#type;
+				},
+				set: function(value) {
+					this.#type = value;
+				}
+			},
+			Element: {
+				get: function() {
+					return this.#element;
+				}
+			}
+		});
+	}
+	
+	#generateToggle(container) {
+		var setting = document.createElement('setting');
+		setting.classList.add('setting-toggle');
+		var name = document.createElement('setting-name');
+		name.textContent = this.Name;
+		var description = document.createElement('span');
+		description.textContent = this.Description;
+		if (this.Value)
+			setting.classList.add('enabled');
+		setting.appendChild(name);
+		setting.appendChild(description);
+		setting.onclick = function(e) {
+			var element = findSetting(this.id);
+			if (element == null)
+				return;				
+			if (!element.Value)
+				this.classList.add('enabled');
+			else 
+				this.classList.remove('enabled');
+			element.Value = !element.Value;
+			UserConfig.changeGoal(element.Id, element.Value);
+		};
+		setting.id = this.Id;
+		this.#element = setting;
+		container.appendChild(setting);
+	}
+	
+	#generateTimeSpan(container) {
+		var setting = document.createElement('setting');
+		setting.classList.add('setting-timespan');
+		var name = document.createElement('setting-name');
+		name.textContent = this.Name;
+		var valueContainer = document.createElement('timespan');
+		
+		var down = document.createElement('interval-down');
+		var downArrow = document.createElement('i');
+		downArrow.classList.add('material-icons');
+		downArrow.textContent = 'arrow_drop_down';
+		down.appendChild(downArrow);
+		down.id = this.Id + '-down';
+		var up = document.createElement('interval-up');
+		var upArrow = document.createElement('i');
+		upArrow.classList.add('material-icons');
+		upArrow.textContent = 'arrow_drop_up';
+		up.appendChild(upArrow);
+		up.id = this.Id + '-up';
+		
+		
+		var value = document.createElement('span');
+		value.textContent = this.Value.Value;
+		value.id = this.Id + '-value';
+		var interval = document.createElement('interval');
+		interval.textContent = this.Value.Interval + '(s)';
+		interval.id = this.Id + '-interval';
+				
+		down.onclick = function(e) {
+			var element = findSetting(this.id.split('-')[0]);
+			if (element == null)
+				return;
+			if (element.Value.Interval.toLowerCase() == 'hour')
+				element.Value.Value = String(parseInt(element.Value.Value) - 1);
+			else 
+				element.Value.Value = String(parseInt(element.Value.Value) - 5);
+			var valueElement = document.getElementById(element.Id + '-value');
+			valueElement.textContent = element.Value.Value;
+			UserConfig.changeParameter(element.Id, element.Value);
+		}
+		
+		up.onclick = function(e) {
+			var element = findSetting(this.id.split('-')[0]);
+			if (element == null)
+				return;
+			if (element.Value.Interval.toLowerCase() == 'hour') {
+				element.Value.Value = String(parseInt(element.Value.Value) + 1);
+			}
+			else 
+				element.Value.Value = String(parseInt(element.Value.Value) + 5);
+			var valueElement = document.getElementById(element.Id + '-value');
+			valueElement.textContent = element.Value.Value;
+			UserConfig.changeParameter(element.Id, element.Value);
+		}
+		
+		interval.onclick = function(e) {
+			// toggle minute/hour and save
+			var element = findSetting(this.id.split('-')[0]);
+			if (element == null)
+				return;
+			if (element.Value.Interval.toLowerCase() == 'hour')
+				element.Value.Interval = 'Minute';
+			else 
+				element.Value.Interval = 'Hour';
+			this.textContent = element.Value.Interval + '(s)';
+			UserConfig.changeParameter(element.Id, element.Value);
+		};
+		
+		valueContainer.appendChild(down);
+		valueContainer.appendChild(value);
+		valueContainer.appendChild(up);
+		valueContainer.appendChild(interval);
+		setting.appendChild(name);
+		setting.appendChild(valueContainer);
+		setting.id = this.Id;
+		this.#element = setting;
+		container.appendChild(setting);
+	}
+	
+	generate(container) {
+		switch (this.Type) {
+			case ToggleType:
+				this.#generateToggle(container);
+				break;
+			default:
+				this.#generateTimeSpan(container);
+				break;
+		}
+	}
+}
+
+class SettingGroup 
+{
+	#name;
+	#settings;
+	#element;
+		
+	constructor(name) 
+	{
+		this.#name = name;
+		
+		Object.defineProperties(this, {
+			Name: {
+				get: function() {
+					return this.#name;
+				},
+				set: function(value) {
+					this.#name = value;
+				}
+			},
+			Settings: {
+				get: function() {
+					return this.#settings;
+				},
+				set: function(value) {
+					this.#settings = value;
+				}
+			}
+		});
+	}
+	
+	generate() {
+		var element = document.createElement('setting-group');
+		var title = document.createElement('h2');
+		title.textContent = this.Name;
+		element.appendChild(title);
+		this.#element = element;
+		container.appendChild(element);
+		
+		this.Settings.forEach(s => s.generate(this.#element));
+	}
+}
+
+setupSettings = function() {
+	goalGroup = new SettingGroup('Goals');
+	goalGroup.Settings = Object.entries(AppConfig.Goals).sort(g => g.Sort).map(g => {
+		return new Setting(g[0], g[1].Label, g[1].Description, UserConfig.Goals.find(u => u == g[0]) != null, GoalCategory, ToggleType);
+	});
+	goalGroup.generate();
+	
+	parameterGroup = new SettingGroup('Parameters');
+	parameterGroup.Settings = Object.entries(AppConfig.Parameters).sort(p => p[0]).map(p => {
+		return new Setting(
+			p[0], 
+			p[1].Label, 
+			p[1].Description, 
+			{
+				Value: UserConfig.Parameters[p[0]] != null ? UserConfig.Parameters[p[0]].Value : p[1].DefaultValue,
+				Interval: UserConfig.Parameters[p[0]] != null ? UserConfig.Parameters[p[0]].Interval : p[1].DefaultInterval
+			}, 
+			ParameterCategory, 
+			TimeSpanType
+		);
+	});
+	parameterGroup.generate();
+}
