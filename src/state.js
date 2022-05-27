eaebd89e2cbf7674872980523d447742db4deec6
Devin Burke <devinburke@gmail.com>
2022-05-26 22:51:32 -0500

    intermedia change while rewriting much of the application

--- src/state.js
+++ src/state.js
@@ -7,9 +7,6 @@ class StateManager {
         this.visualizationManager = visualizationManager
         this.refreshInterval = refreshInterval
         this.ruleEngine = ruleEngine
-        this.ruleEngine.assignArgumentFunctions(this.functionFactory())
-        this.snapshot = {}
-        this.#refreshSnapshot()
     }
 
     async start() {
@@ -42,75 +39,49 @@ class StateManager {
                     this.ruleEngine.removeTriggerHolds(this.currentState)
                 }
             }
-            
-            if (this.activity.schedule.isScheduledOn(getNow()))
-                this.ruleEngine.run()
 
-            this.visualizationManager.setData(this.getAnalyticsData())
+            const activitySummary = this.activity.getSummary()
+            if (this.activity.schedule.isScheduledOn(getNow()))
+                this.ruleEngine.run(activitySummary)
+
+            this.visualizationManager.setData(activitySummary)
             
             setTimeout((async() => await this.run()), this.refreshInterval)
         }
     }
 
-    functionFactory() {
-        return {
-            is_state: (arg) => arg.includes(this.currentState),
-            from_schedule: (arg) => this.activity.schedule.toMap(getNow())[arg[0]],
-            daily_time: (arg) => this.activity.getMinutesInState(arg),
-            maximum_daily_time: (arg) => this.activity.getMaximumPossibleMinutesInState(arg),
-            time_since: (arg) => this.activity.getMinutesSinceState(arg)
-        }
-    }
-
-    #refreshSnapshot() {
-        this.snapshot['timestamp'] = null
-        this.snapshot['rawState'] = null
-        this.snapshot['assumedState'] = null
-        this.snapshot['scheduleStart'] = null
-        this.snapshot['scheduleEnd'] = null
-        this.snapshot['stateChanges'] = [
-            {'timestamp': null, 'state': 'Sitting'},
-            {'timestamp': null, 'state': 'Sitting'},
-            {'timestamp': null, 'state': 'Sitting'}
-        ]
-        this.snapshot['parameters'] = {
-            'stand-goal': 0,
-            'work-goal': 0,
-        }
-    }
-
-    getAnalyticsData() {
-        const hoursWorked = this.activity.getMinutesInState(['Standing', 'Sitting']) / 60.00
-        const hoursGoal = this.ruleEngine.getParameterInHours('work_day_minimum')
-        const hoursStood = this.activity.getMinutesInState(['Standing']) / 60.00
-        const standGoal = this.ruleEngine.getParameterInHours('stand_day_minimum')
+    // getAnalyticsData() {
+    //     const hoursWorked = this.activity.getMinutesInState(['Standing', 'Sitting']) / 60.00
+    //     const hoursGoal = this.ruleEngine.getParameterInHours('param-work-day-minimum')
+    //     const hoursStood = this.activity.getMinutesInState(['Standing']) / 60.00
+    //     const standGoal = this.ruleEngine.getParameterInHours('param-stand-day-minimum')
         
-        return {
-            'Data Points': {
-                'Hours Standing': hoursStood,
-                'Hours Working': hoursWorked,
-                'Day Hours Remaining': this.activity.getMinutesRemainingInDay() / 60.00,
-                'Stand Hours Remaining': Math.max(standGoal - hoursStood, 0),
-                'Work Hours Remaining': Math.max(hoursGoal - hoursWorked, 0),
-                'Maximum Break Time': this.ruleEngine.getParameterInHours('away_now_maximum') * 60.0,
-                'Minimum Hours Standing': standGoal,
-                'Minimum Hours Working': this.ruleEngine.getParameterInHours('work_day_minimum'),
-                'Current State': this.activity.getCurrentState(true),
-                'Raw State': this.activity.getCurrentState(false),
-                'Breaks Taken': this.activity.getStateOccurrences(['Absent']).map(o => {
-                    const obj = {}
-                    obj[o.timestamp.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })] = o.duration
-                    return obj
-                }),
-            },
-            'Goal Progress': {
-                //'Ensure I work a full day': 'In Progress',
-                //'Stop working the earliest I can': 'In Progress',
-                //'Stand a minimum amount each day': 'Completed',
-                //'Regularly switch to standing': 'Disabled',
-                //'Regularly take a break and walk away': 'Failed',
-                //'Ensure I am never away too long': 'Failed'
-            }
-        }
-    }
+    //     return {
+    //         'Data Points': {
+    //             'Hours Standing': hoursStood,
+    //             'Hours Working': hoursWorked,
+    //             'Day Hours Remaining': this.activity.getMinutesRemainingInDay() / 60.00,
+    //             'Stand Hours Remaining': Math.max(standGoal - hoursStood, 0),
+    //             'Work Hours Remaining': Math.max(hoursGoal - hoursWorked, 0),
+    //             'Maximum Break Time': this.ruleEngine.getParameterInHours('param-absent-now-maximum') * 60.0,
+    //             'Minimum Hours Standing': standGoal,
+    //             'Minimum Hours Working': this.ruleEngine.getParameterInHours('param-work-day-minimum'),
+    //             'Current State': this.activity.getCurrentState(true),
+    //             'Raw State': this.activity.getCurrentState(false),
+    //             'Breaks Taken': this.activity.getStateOccurrences(['Absent']).map(o => {
+    //                 const obj = {}
+    //                 obj[o.timestamp.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })] = o.duration
+    //                 return obj
+    //             }),
+    //         },
+    //         'Goal Progress': {
+    //             //'Ensure I work a full day': 'In Progress',
+    //             //'Stop working the earliest I can': 'In Progress',
+    //             //'Stand a minimum amount each day': 'Completed',
+    //             //'Regularly switch to standing': 'Disabled',
+    //             //'Regularly take a break and walk away': 'Failed',
+    //             //'Ensure I am never away too long': 'Failed'
+    //         }
+    //     }
+    // }
 }
