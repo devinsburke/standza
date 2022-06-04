34b3d83db684fa86516318fd0f2de28c4926a0d4
Devin Burke <devinburke@gmail.com>
2022-06-04 10:28:43 -0500

    added bootstrap and initial context bridge functions; not perfect yet because camera has async race condition that i need to fix

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, screen} = require('electron')
 const path = require('path')
 const PiCamera = require('pi-camera')
-const fs = require('fs').promises;
+const fs = require('fs').promises
+const bootstrap = require('./data/bootstrap.json')
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 480,
-    webPreferences: {
-      nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  mainWindow.loadFile('index.html')
+    // Create the browser window.
+    let display
+    if (bootstrap.window.externalDisplay)
+        display = screen.getAllDisplays().find(d => d.bounds.x !== 0 || d.bounds.y !== 0)
+
+    const mainWindow = new BrowserWindow({
+        width: bootstrap.window.width,
+        height: bootstrap.window.height,
+        autoHideMenuBar: bootstrap.window.autoHideMenuBar,
+        frame: bootstrap.window.frame,
+        x: bootstrap.window.x + (display ? display.bounds.x : 0),
+        y: bootstrap.window.y + (display ? display.bounds.y : 0),
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile(bootstrap.mainDocument)
+    if (bootstrap.window.fullScreen)
+        mainWindow.setFullScreen(true)
+    if (bootstrap.window.developerTools)
+        mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
-  app.on('activate', function () {
+    createWindow();
+    app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  });
+    });
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
--- preload.js
+++ preload.js
@@ -1,10 +1,41 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 
+const PiCamera = require('pi-camera')
 const fs = require('fs').promises
 const { contextBridge, ipcRenderer } = require('electron')
 
-window.addEventListener('DOMContentLoaded', () => {
+const webgl = require('@tensorflow/tfjs-backend-webgl')
+const tf = require('@tensorflow/tfjs-core')
+const posenet = require('@tensorflow-models/posenet')
+
+let picam
+let net
+
+contextBridge.exposeInMainWorld('tensorflow', {
+  estimatePose: async(img) => {
+    const input = tf.browser.fromPixels(img)
+		const poses = (await net).estimateSinglePose(input)
+    input.dispose()
+    return await poses
+  }
+})
+
+window.addEventListener('DOMContentLoaded', async () => {
+  net = posenet.load()
+  
+  picam = new PiCamera({ 
+    mode: 'photo',
+    width: 640, 
+    height: 480, 
+    nopreview: true,
+    timeout: 1
+  });
+  
+  contextBridge.exposeInMainWorld('picamera', {
+    snapDataUrl: () => picam.snapDataUrl()
+  });
+  
   for (const t of ['chrome', 'node', 'electron']) {
     const element = document.getElementById(`${t}-version`)
     if (element) element.innerText = process.versions[t]
